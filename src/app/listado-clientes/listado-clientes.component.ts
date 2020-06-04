423661c25ea51e39b0499a4a2e8ab920782fc4ce
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-06-04 15:41:07 -0300

    ngOnInit updated: ForEach for cliente of clientes

--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -10,12 +10,34 @@ export class ListadoClientesComponent implements OnInit {
   clientes: any[] = new Array<any>();
 
   constructor(private firestore: AngularFirestore) { 
-    this.firestore.collection('clientes').valueChanges().subscribe((resultado)=>{
-      this.clientes = resultado;
-    })
+
   }
 
   ngOnInit(): void {
-  }
+    // this.firestore.collection('clientes').valueChanges().subscribe((resultado)=>{
+    //   this.clientes = resultado;
+    // })
+    this.clientes.length = 0;
+    this.firestore.collection('clientes').get().subscribe((resultado)=>{
+      // console.log(resultado.docs);
+      
+      //FOR STANDAR
+      // for(let item of resultado.docs){
+      //   console.log(item.id);
+      //   console.log(item.ref);
+      //   console.log(item.data());        
+      // }
 
+      //FOR EACH
+      resultado.docs.forEach(item =>{
+        // console.log(item.id);
+        // console.log(item.ref);
+        // console.log(item.data()); 
+        let cliente = item.data();
+        cliente.id = item.id;
+        cliente.ref = item.ref;
+        this.clientes.push(cliente);
+      })
+    })
+  }
 }
