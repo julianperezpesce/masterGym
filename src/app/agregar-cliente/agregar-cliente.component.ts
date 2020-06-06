421ab100dabb4cb2aa480a3d02ab916d806c319f
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-06-05 21:57:37 -0300

    angular firestore collection added

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -10,8 +11,10 @@ import { AngularFireStorage } from '@angular/fire/storage';
 export class AgregarClienteComponent implements OnInit {
   formularioCliente: FormGroup;
   porcentajeImagen: number = 0;
-
-  constructor(private fb: FormBuilder, private storage: AngularFireStorage) {
+  urlImagen: string = '';
+  constructor(private fb: FormBuilder, 
+    private storage: AngularFireStorage,
+    private afs: AngularFirestore) {
 
    }
 
@@ -30,22 +33,28 @@ export class AgregarClienteComponent implements OnInit {
   }
 
   agregar(){
-    console.log(this.formularioCliente.value);
-    
+    this.formularioCliente.value.imgUrl = this.urlImagen;
+    this.afs.collection('clientes').add(this.formularioCliente.value);
   }
 
   subirImagen(event){
-    let nombreImagen = new Date().getTime().toString();
-    const file = event.target.files[0];    
-    let fileExtension = file.name.toString().substring(file.name.toString().lastIndexOf('.'))
-    const filePath = `imagenesDeClientes/${nombreImagen}${fileExtension}`;
-    const ref = this.storage.ref(filePath);
-    const task = ref.put(file);
-    task.percentageChanges().subscribe((porcentaje)=>{
-      this.porcentajeImagen = parseInt(porcentaje.toString());
-      
-    })
-
+    if (event.target.files.length > 0) {
+      let nombreImagen = new Date().getTime().toString();
+      const file = event.target.files[0];    
+      let fileExtension = file.name.toString().substring(file.name.toString().lastIndexOf('.'))
+      const filePath = `imagenesDeClientes/${nombreImagen}${fileExtension}`;
+      const ref = this.storage.ref(filePath);
+      const task = ref.put(file);
+      task.then((object)=>{
+        console.log('imagen cargada');
+        
+        ref.getDownloadURL().subscribe((url)=>{
+          this.urlImagen = url;          
+        })
+      })
+      task.percentageChanges().subscribe((porcentaje)=>{
+        this.porcentajeImagen = parseInt(porcentaje.toString());      
+      })      
+    }
   }
-
 }
