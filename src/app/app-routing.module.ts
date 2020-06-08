25fa5cdb28fbb713bb7daec6d6e893ef070211e0
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-06-08 12:03:28 -0300

    new component precios + app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component';
+import { PreciosComponent } from './precios/precios.component';
 
 
 const routes: Routes = [
   {path: 'listado-clientes', component: ListadoClientesComponent},
   {path: 'agregar-cliente', component: AgregarClienteComponent},
   {path: 'agregar-cliente/:clienteID', component: AgregarClienteComponent},
-
+  {path: 'precios', component: PreciosComponent},
 ];
 
 @NgModule({
