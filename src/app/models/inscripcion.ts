a3dc5fa76c6a3d859ae7065eadb75c6fcd38680a
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-06-09 20:48:59 -0300

    Class Inscripcion.ts updated

--- src/app/models/inscripcion.ts
+++ src/app/models/inscripcion.ts
@@ -3,6 +3,9 @@ import { DocumentReference } from "@angular/fire/firestore";
 export class Inscripcion {
     fecha: Date;
     fechaDeBaja: Date;
+    cliente: DocumentReference;
     tipoInscripcion: DocumentReference;
-
+    subTotal: number;
+    iva: number;
+    total: number;
 }
