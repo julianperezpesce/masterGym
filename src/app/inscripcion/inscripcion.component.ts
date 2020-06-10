9c7f1e257afca6cd9fb8d34a356b94a53a9e0f81
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-06-10 12:54:03 -0300

    date configured

--- src/app/inscripcion/inscripcion.component.ts
+++ src/app/inscripcion/inscripcion.component.ts
@@ -20,6 +20,7 @@ export class InscripcionComponent implements OnInit {
   constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    //Setting Firestore database
     this.afs.collection('precios').get().subscribe((resultado)=>{
       resultado.docs.forEach((i)=>{
         let precio = i.data() as Precios;
@@ -28,6 +29,7 @@ export class InscripcionComponent implements OnInit {
         this.precios.push(precio)
       })
     })
+    //
   }
 
   asignarCliente(cliente: Clientes){
@@ -48,9 +50,41 @@ export class InscripcionComponent implements OnInit {
     this.precioSeleccionado = this.precios.find(x => x.id == id);
     this.inscripcion.tipoInscripcion = this.precioSeleccionado.ref
     console.log(this.precioSeleccionado);
-
+    //Setting Date
     this.inscripcion.fecha = new Date();
-    
+    if (this.precioSeleccionado.tipoSuscripcion == 1) {
+      let cantidad = this.precioSeleccionado.cantidad;
+      let fechaDeBaja = new Date(
+        this.inscripcion.fecha.getFullYear(),        
+        this.inscripcion.fecha.getMonth() + cantidad,
+        this.inscripcion.fecha.getDate()
+        )
+      this.inscripcion.fechaDeBaja = fechaDeBaja;
+    }else{
+      if (this.precioSeleccionado.tipoSuscripcion == 2) {
+        //Six months it's calculated by months 1 semestre = 6 months
+        let meses: number = this.precioSeleccionado.cantidad + this.inscripcion.fecha.getMonth();
+        let anio: number = this.inscripcion.fecha.getFullYear();
+        let dias: number = this.inscripcion.fecha.getDate();
+        //
+        let fechaDeBaja = new Date(anio, meses, dias);
+        this.inscripcion.fechaDeBaja = fechaDeBaja;
+      }
+    }if (this.precioSeleccionado.tipoSuscripcion == 3) {
+        //Anual it's calculated by months 1 year = 12 months
+        let meses: number = this.precioSeleccionado.cantidad + this.inscripcion.fecha.getMonth();
+        let anio: number = this.inscripcion.fecha.getFullYear();
+        let dias: number = this.inscripcion.fecha.getDate();
+        //
+        let fechaDeBaja = new Date(anio, meses, dias);
+        this.inscripcion.fechaDeBaja = fechaDeBaja;
+    }
+
   }
 
 }
+
+// <option value="1">Mensual</option>
+// <option value="2">Semestral</option>
+// <option value="3">Anual</option>
+// <option value="4">Anualx2</option>
