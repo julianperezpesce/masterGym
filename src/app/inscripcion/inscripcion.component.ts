03eb57a5e3c5b1cc26704953b2d3cbb3c914e2bd
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-06-10 18:09:44 -0300

    validation on Inscripcion of client name & suscrip

--- src/app/inscripcion/inscripcion.component.ts
+++ src/app/inscripcion/inscripcion.component.ts
@@ -43,43 +43,65 @@ export class InscripcionComponent implements OnInit {
   }
 
   guardar(){
-
+    if (this.inscripcion.validar().esValido) {
+      console.log('Guardando');
+      
+    } else {
+      console.log(this.inscripcion.validar().mensaje);
+      
+    }
   }
 
   seleccionarPrecio(id: string){
-    this.precioSeleccionado = this.precios.find(x => x.id == id);
-    this.inscripcion.tipoInscripcion = this.precioSeleccionado.ref
-    console.log(this.precioSeleccionado);
-    //Setting Date
-    this.inscripcion.fecha = new Date();
-    if (this.precioSeleccionado.tipoSuscripcion == 1) {
-      let cantidad = this.precioSeleccionado.cantidad;
-      let fechaDeBaja = new Date(
-        this.inscripcion.fecha.getFullYear(),        
-        this.inscripcion.fecha.getMonth() + cantidad,
-        this.inscripcion.fecha.getDate()
-        )
-      this.inscripcion.fechaDeBaja = fechaDeBaja;
-    }else{
-      if (this.precioSeleccionado.tipoSuscripcion == 2) {
-        //Six months it's calculated by months 1 semestre = 6 months
-        let meses: number = this.precioSeleccionado.cantidad + this.inscripcion.fecha.getMonth();
-        let anio: number = this.inscripcion.fecha.getFullYear();
-        let dias: number = this.inscripcion.fecha.getDate();
-        //
-        let fechaDeBaja = new Date(anio, meses, dias);
+    if (id != "null") {
+      this.precioSeleccionado = this.precios.find(x => x.id == id);
+      this.inscripcion.tipoInscripcion = this.precioSeleccionado.ref;
+            
+      //Calculate subtotal, IVA, total
+      this.inscripcion.subTotal = this.precioSeleccionado.costo;
+      this.inscripcion.iva = this.inscripcion.subTotal * 0.21;
+      this.inscripcion.total = this.inscripcion.subTotal + this.inscripcion.iva;
+      
+      //Setting Date
+      this.inscripcion.fecha = new Date();
+      if (this.precioSeleccionado.tipoSuscripcion == 1) {
+        let cantidad = this.precioSeleccionado.cantidad;
+        let fechaDeBaja = new Date(
+          this.inscripcion.fecha.getFullYear(),        
+          this.inscripcion.fecha.getMonth() + cantidad,
+          this.inscripcion.fecha.getDate()
+          )
         this.inscripcion.fechaDeBaja = fechaDeBaja;
+      }else{
+        if (this.precioSeleccionado.tipoSuscripcion == 2) {
+          //Six months it's calculated by months 1 semestre = 6 months
+          let meses: number = this.precioSeleccionado.cantidad + this.inscripcion.fecha.getMonth();
+          let anio: number = this.inscripcion.fecha.getFullYear();
+          let dias: number = this.inscripcion.fecha.getDate();
+          //
+          let fechaDeBaja = new Date(anio, meses, dias);
+          this.inscripcion.fechaDeBaja = fechaDeBaja;
+        }
+      } if (this.precioSeleccionado.tipoSuscripcion == 3) {
+          //Anual it's calculated by months 1 year = 12 months
+          let meses: number = this.precioSeleccionado.cantidad + this.inscripcion.fecha.getMonth();
+          let anio: number = this.inscripcion.fecha.getFullYear();
+          let dias: number = this.inscripcion.fecha.getDate();
+          //
+          let fechaDeBaja = new Date(anio, meses, dias);
+          this.inscripcion.fechaDeBaja = fechaDeBaja;
       }
-    }if (this.precioSeleccionado.tipoSuscripcion == 3) {
-        //Anual it's calculated by months 1 year = 12 months
-        let meses: number = this.precioSeleccionado.cantidad + this.inscripcion.fecha.getMonth();
-        let anio: number = this.inscripcion.fecha.getFullYear();
-        let dias: number = this.inscripcion.fecha.getDate();
-        //
-        let fechaDeBaja = new Date(anio, meses, dias);
-        this.inscripcion.fechaDeBaja = fechaDeBaja;
-    }
+    }else{
 
+      //Reset the values to null when user choose Suscripcion on select menu
+      this.precioSeleccionado = new Precios();
+      this.inscripcion.tipoInscripcion = null;  
+      this.inscripcion.fecha = null;
+      this.inscripcion.fechaDeBaja = null;    
+      this.inscripcion.subTotal = null;
+      this.inscripcion.iva = null;
+      this.inscripcion.total = null;
+    }
   }
 
 }
--- src/app/models/inscripcion.ts
+++ src/app/models/inscripcion.ts
@@ -18,4 +18,28 @@ export class Inscripcion {
         this.iva = this.iva;
         this.total = this.total;
     }
+
+
+    validar(): any{
+        let respuesta = {
+            esValido: false,
+            mensaje: ''
+        }
+
+        //Validation of required Client name and Suscription
+        if (this.tipoInscripcion == null || this.tipoInscripcion == undefined) {
+            respuesta.esValido = false;
+            respuesta.mensaje = 'Debe elegir una suscripción';
+            return respuesta;
+        }
+
+        if (this.cliente == null || this.cliente == undefined) {
+            respuesta.esValido = false;
+            respuesta.mensaje = 'Debe elegir un cliente';
+            return respuesta;
+        }
+
+        respuesta.esValido = true;
+        return respuesta;
+    }
 }
